3eea90fec3966616b026f0187968a8e05d0f4fa8
MicaelSv <micaelsilva012@hotmail.com>
2025-04-21 17:45:45 -0300

    register-usuario

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -19,3 +19,12 @@ def read_root():
     return JSONResponse(content={"message": "Hello from FastAPI on Vercel!"})
 
 
+# Nova rota para receber os dados do formulário
+@app.post("/cadastro")
+async def receber_cadastro(request: Request):
+    dados = await request.json()
+    print("📥 Dados recebidos do frontend:")
+    for chave, valor in dados.items():
+        print(f"{chave}: {valor}")
+    
+    return JSONResponse(content={"status": "sucesso", "mensagem": "Dados recebidos com sucesso!"})
