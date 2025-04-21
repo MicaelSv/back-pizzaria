9c6374e91b8555a070ece6b3bf72f29c317512a1
MicaelSv <micaelsilva012@hotmail.com>
2025-04-21 17:24:34 -0300

    att

--- api/main.py
+++ api/main.py
@@ -3,6 +3,11 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/msg")
 def read_root():
     return JSONResponse(content={"message": "Hello from FastAPI on Vercel!"})
+
+
+@app.get("/teste")
+def read_roo2t():
+    return JSONResponse(content={"a":"!!!!!"})
