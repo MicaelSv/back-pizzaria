cb46966d929c75d92668789643c3ed4faccea4c5
MicaelSv <micaelsilva012@hotmail.com>
2025-04-21 17:30:44 -0300

    erro-cors

--- api/main.py
+++ api/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configurando o CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou substitua com a URL do frontend em produção
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/msg")
 def read_root():
     return JSONResponse(content={"message": "Hello from FastAPI on Vercel!"})
 
 
-@app.get("/teste")
-def read_roo2t():
-    return JSONResponse(content={"a":"!!!!!"})
